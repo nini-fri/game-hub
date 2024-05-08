c716955765db59f1847162f015efa1d9bd3a6138
jiahuizheng <ninhuijia@gmail.com>
2024-05-08 17:33:31 +0100

    create a generic data fetching hook and display genres image

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -5,16 +5,16 @@ import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
-  const { games, error, isLoading } = useGames();
+  const { data, error, isLoading } = useGames();
   const skeletons = [1, 2, 3, 4, 5, 6];
 
   return (
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid
-        columns={{ sm: 2, md: 2, lg: 3, xl: 4 }}
-        padding="10px"
-        spacing={10}
+        columns={{ sm: 2, md: 2, lg: 5, xl: 4 }}
+        padding="5px"
+        spacing={3}
       >
         {isLoading &&
           skeletons.map((skeleton) => (
@@ -22,7 +22,7 @@ const GameGrid = () => {
           <GameCardSkeleton />
           </GameCardContainer>
         ))}
-        {games.map((game) => (
+        {data.map((game) => (
           <GameCardContainer key={game.id}>
           <GameCard  game={game} />
           </GameCardContainer>
