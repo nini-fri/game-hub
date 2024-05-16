7f3d12ba7faf3f073c03ec1aa0c3c5030a98cdff
jiahuizheng <ninhuijia@gmail.com>
2024-05-16 21:20:00 +0100

    error page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import GameDetailPage from "./pages/GameDetailPage";
 import HomePage from "./pages/HomePage";
+import ErrorPage from "./pages/ErrorPage";
 import Layout from "./pages/Layout";
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       { path: 'games/:id', element: <GameDetailPage /> }
